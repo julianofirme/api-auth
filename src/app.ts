dfc39f1eb5ffb9d9067cd4813e243208c2a704f6
Juliano Firme <Juliano Firme>
2021-10-27 20:48:41 -0300

    criando conexão com o mongodb e pequeno fix no docker-compose

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,12 @@ import * as express from "express";
 import * as cors from "cors";
 import * as logger from "morgan";
 
+import { connectToDb } from "./config/db";
+
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(logger("dev"));
+
+connectToDb();
---
+++ src/config/db.ts
@@ -0,0 +1,10 @@
+import { createConnection } from "typeorm";
+
+export const connectToDb = async () => {
+  const connect = await createConnection()
+  console.log(`:::App connect to db -> ${connect.options.database}`)
+
+  process.on('SIGINT', () => {
+    connect.close().then(() => console.log('Db connection closed'))
+  })
+}
