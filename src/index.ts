b0b0372f4243bee4574cf6bc4d1d32ce53d6ad64
Juliano Firme <Juliano Firme>
2021-10-27 19:41:03 -0300

    inicializando o projeto typeorm + mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import "reflect-metadata";
-import express from "express";
-import routes from './routes'
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
 
-const app = express();
-const PORT = 8080
+createConnection().then(async connection => {
 
-app.use(express.json());
-app.use(routes)
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-app.get("/", (req, res) => {
-  res.send("hello")
-})
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
 
-app.listen(PORT, () => console.log(`:::Server run on port: ${PORT}`))
+    console.log("Here you can setup and run express/koa/any other framework.");
+
+}).catch(error => console.log(error));
